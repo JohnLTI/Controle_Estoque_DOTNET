931e5a59043f9b72728857f15b754a4a96389b04
JohnLTI <xjl16@outlook.com>
2023-12-03 19:24:56 -0300

    Criação Classes, Atrbutos e diagrama

--- Controle_Estoque_API/Models/Produto.cs
+++ Controle_Estoque_API/Models/Produto.cs
@@ -1,6 +1,21 @@
-﻿namespace Controle_Estoque_API.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Controle_Estoque_API.Models
 {
     public class Produto
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [DisplayName("Preço")]
+        [RegularExpression(@"^R$ \d{1,3}(?:.\d{3})*(?:,\d{2})?$")]
+        public decimal PrecoUnit { get; set; }
     }
 }
