98a32cced144771f7c650c209b57e59c3a9ca994
emmanuel <emman@DESKTOP-ECMB23>
2023-12-13 19:02:17 -0300

    Criando Context, Readme e atualizando a Model

---
+++ Estoque_API/Context/EstoqueDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Estoque_API.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Estoque_API.Context
+{
+    public class EstoqueDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        // Configuração do MySQL
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseMySql("conexao_aqui");*/
+    
+    }
+}
